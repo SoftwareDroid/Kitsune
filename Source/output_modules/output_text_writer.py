017dfad9fe1bd2245da73f90df2c0bcf9bdb1c9e
patrick <patrickmis@web.de>
2022-06-19 10:58:14 +0200

    small fix

--- Source/output_modules/output_text_writer.py
+++ Source/output_modules/output_text_writer.py
@@ -12,7 +12,8 @@ def create_text_output(sentences: Sequence[Tuple[str, Sequence[JapWord]]], setti
     now = datetime.datetime.now()
     input_file = settings["input"]["filename"]
     timestamp = now.strftime("%d.%m.%Y %H:%M:%S")
-    practice_sheet = f"{title1}\nInput: {input_file}\nKitsune Version: {version}\nTimestamp: {timestamp}\nSentences\n\n"
+    lines = settings["input"]["lines"]
+    practice_sheet = f"{title1}\nInput: {input_file}\nLines: {lines}\nKitsune Version: {version}\nTimestamp: {timestamp}\nSentences\n\n"
     counter = 0
     for sentence in sentences:
         copy: str = sentence[0]
@@ -29,11 +30,15 @@ def create_text_output(sentences: Sequence[Tuple[str, Sequence[JapWord]]], setti
         counter += 1
     # create cheat sheet
     title2 = settings["output"]["word_sheet"]
-    cheat_sheet = f"{title2}\nInput: {input_file} \nKitsune Version: {version}\nTimestamp: {timestamp}\n\nWords:\n"
+    cheat_sheet = f"{title2}\nInput: {input_file}\nLines: {lines} \nKitsune Version: {version}\nTimestamp: {timestamp}\n\nWords:\n"
 
     for link in linking:
         entry: JapWord = linking[link][1]
         references = linking[link][0]
-        cheat_sheet += f"{entry.jap()} ({entry.hiragana()}) \n   {entry.description()} \n   {references} \n"
+
+        if entry.jap() != entry.hiragana():
+            cheat_sheet += f"{entry.jap()} ({entry.hiragana()}) \n   {entry.description()} \n   {references} \n"
+        else:
+            cheat_sheet += f"{entry.jap()}\n   {entry.description()} \n   {references} \n"
 
     return practice_sheet, cheat_sheet
